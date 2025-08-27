b4107cb674a1bb978f1479923d94386b9ed13c02
suhail7868 <63796357+suhail7868@users.noreply.github.com>
2025-08-27 13:33:31 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+# from setuptools import find_packages,setup
+# from typing import List
+
+
+# def get_requirements(file_path:str)->List[str]:
+#     requirements=[]
+#     with open(file_path) as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements=[req.replace("\n","") for req in requirements]
+#         return requirements
+    
+
+
+# setup(
+#     name="DiamondPricePrediction",
+#     version='0.0.1',
+#     author='suhail',
+#     author_email="suhail98ever@gmail.com",
+#     install_requires=get_requirements('requirements.txt'),
+#     package=find_packages(),
+# )
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        # Remove the -e . line if it exists
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+            
+        return requirements
+
+setup(
+    name="DiamondPricePrediction",
+    version='0.0.1',
+    author='suhail',
+    author_email="suhail98ever@gmail.com",
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages(),
+)
